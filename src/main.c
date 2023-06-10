488b55e9d71a2be708b5f78d15e299ba243f6a3e
Jitain Sangwan <jitain.sangwan1997@gmail.com>
2023-06-11 01:44:24 +0530

    Currently under Progress

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@ int main()
 {
 //    int8_t response[100] = "ATI\r\nQuectel\r\nEC200U\r\nRevision: EC200UCNAAR01A04M08\r\nOK\r\n";
     int8_t response[100] ="+COPS: 0,0,\"CHN-UNICOM\",7\r\n\r\nOK";
-    uint8_t dummy;
     Parse_Response(response);
 
 return 0;
